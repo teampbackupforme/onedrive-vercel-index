f82c41aaa1b764c2d74eef775910597c30527aa4
teampbackupforme <111464989+teampbackupforme@users.noreply.github.com>
2022-08-18 18:13:04 -0500

    Update api.config.js

--- config/api.config.js
+++ config/api.config.js
@@ -10,8 +10,8 @@
 module.exports = {
   // The clientId and clientSecret are used to authenticate the user with Microsoft Graph API using OAuth. You would
   // not need to change anything here if you can authenticate with your personal Microsoft account with OneDrive International.
-  clientId: 'd87bcc39-1750-4ca0-ad54-f8d0efbb2735',
-  obfuscatedClientSecret: 'U2FsdGVkX1830zo3/pFDqaBCVBb37iLw3WnBDWGF9GIB2f4apzv0roemp8Y+iIxI3Ih5ecyukqELQEGzZlYiWg==',
+  clientId: 'dca1d788-e0c0-4245-a214-7fe5ea3821dc',
+  obfuscatedClientSecret: 'U2FsdGVkX19JDFDwbuiU20P910QCygqiDbIm9Z4LKAp87D8f8fb70VMvq+2m9GG6nenY6Ly+fcZ3XdsZVF95gA==',
 
   // The redirectUri is the URL that the user will be redirected to after they have authenticated with Microsoft Graph API.
   // Likewise, you would not need to change redirectUri if you are using your personal Microsoft account with OneDrive International.
